0916952c55e74e6f15e6dc34ee1f66007fe0d147
Antonio Barrios <acbleguizamon@gmail.com>
2023-05-28 18:06:10 -0400

    Nuevo Comentario

---
+++ src/sorteocoop/Sorteocoop.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sorteocoop;
+
+/**
+ *
+ * @author gerencia1
+ */
+public class Sorteocoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    }
+}
